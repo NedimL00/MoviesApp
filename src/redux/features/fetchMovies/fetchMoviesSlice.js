e1875cc57d93db516d2461e5e38bc90a63144c7d
NedimL00 <lapo.nedim@gmail.com>
2022-09-27 16:24:25 +0200

    Fixed https error

--- src/redux/features/fetchMovies/fetchMoviesSlice.js
+++ src/redux/features/fetchMovies/fetchMoviesSlice.js
@@ -11,7 +11,7 @@ const initialState = {
 export const fetchMovies = createAsyncThunk('movies/fetchMovies', async(text)=>{
 
   try {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
       params: {
         apikey: 'dd117956',
         s: text.toLowerCase().split(' ').join('+'),
